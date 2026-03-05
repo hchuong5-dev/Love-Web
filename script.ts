47772b84ed99b6bb2db386ecb40d7e0dcad32b9f
hchuong5-dev <hchuong5@uw.edu>
2026-03-04 19:48:36 -0800

    Add files via upload

---
+++ script.ts
@@ -0,0 +1,50 @@
+// FIRST DAY YOU STARTED DATING
+const startDate = new Date("2024-02-14");
+
+// NEXT MEETING DATE
+const meetDate = new Date("2026-04-10");
+
+const today = new Date();
+
+
+// DAYS TOGETHER
+const diff = today - startDate;
+
+const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+
+document.getElementById("days").textContent = days;
+
+
+
+// COUNTDOWN TO MEET
+const diffMeet = meetDate - today;
+
+const meetDays = Math.floor(diffMeet / (1000 * 60 * 60 * 24));
+
+document.getElementById("meet").textContent =
+"Next time we meet in " + meetDays + " days 💕";
+
+
+
+// IMAGE UPLOAD PREVIEW
+const upload = document.getElementById("upload");
+
+upload.addEventListener("change", function(){
+
+const file = this.files[0];
+
+const reader = new FileReader();
+
+reader.onload = function(e){
+
+const img = document.createElement("img");
+
+img.src = e.target.result;
+
+document.getElementById("gallery").appendChild(img);
+
+}
+
+reader.readAsDataURL(file);
+
+});
